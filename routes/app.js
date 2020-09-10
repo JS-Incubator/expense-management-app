52153605a220e6cbbf69e877742b7b5472c825a3
Janith Olagama <janitholagama.info@gmail.com>
2020-09-10 13:45:01 +0400

    Revert "feast: Add sign-up API"
    This reverts commit 7c5bf5bef48d7943dbcdf13df56cbf7fea7911cd.

--- routes/app.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const {getDashboard} = require('../controller/appController');
-
-//@desc Render the dashboard
-//@route GET /app/dashboard
-router.get('/dashboard', getDashboard);
-
-module.exports = router;
