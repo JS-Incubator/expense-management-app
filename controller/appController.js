c9f35631c34ca6a08b8a19855c58dd23859a4545
Janith Olagama <janitholagama.info@gmail.com>
2020-09-11 17:20:09 +0400

    feast : Add, add new account functionality
    New account can be added, by clicking the floating action button. Data will be saved to mongo-db back-end and redirected to account management scree.

--- controller/appController.js
+++ controller/appController.js
@@ -1,5 +1,51 @@
+const Account = require('../models/account');
+const account = require('../models/account');
+
 module.exports = {
   getDashboard: (req, res) => {
-    res.render('app/dashboard', {pageTitle: 'Dashboard'});
+    res.render('app/dashboard', {
+      pageTitle: 'Dashboard',
+    });
+  },
+
+  getAccountManagement: (req, res) => {
+    res.render('app/account-management.ejs', {pageTitle: 'Account Management'});
+  },
+
+  getAccounts: async (req, res) => {
+    const accounts = await Account.find();
+    //Filtering the account types, Will be moved to its own modal later
+    const accountTypes = [];
+    accounts.forEach((account) => {
+      const isUnique = (type) => type === account.accountType;
+      if (accountTypes.findIndex(isUnique)) {
+        accountTypes.push(account.accountType);
+      }
+    });
+    res.render('app/account-management', {
+      pageTitle: 'Account Managememt',
+      accountTypes,
+      accounts,
+    });
+  },
+
+  postAddAccount: async (req, res) => {
+    const account = {
+      accountName: req.body.accountName,
+      accountType: req.body.accountType,
+      openingDate: req.body.openingDate,
+      openingBalance: req.body.openingBalance
+        ? parseFloat(req.body.openingBalance)
+        : 0,
+      currentBalance: parseFloat(req.body.currentBalance),
+      defaultCurrency: req.body.currency,
+    };
+
+    try {
+      await Account.create(account);
+      res.redirect('/app/accounts');
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
---
+++ models/account.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const {ref} = require('joi');
+
+const AccountSchema = new mongoose.Schema({
+  accountName: {
+    type: String,
+    required: true,
+  },
+  accountType: {
+    type: String,
+    required: true,
+  },
+  openingDate: {
+    type: Date,
+    required: true,
+  },
+  openingBalance: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  currentBalance: {
+    type: Number,
+    required: true,
+  },
+  defaultCurrency: {
+    type: String,
+    required: true,
+  },
+  //   user: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: 'User',
+  //   },
+});
+
+module.exports = mongoose.model('Account', AccountSchema);
