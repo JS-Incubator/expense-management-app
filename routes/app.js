fc10575e3e1fb9301b66e0683c2cb2112307e75d
Janith Olagama <janitholagama.info@gmail.com>
2020-09-11 17:21:17 +0400

    refactor : Refactor some code blocks

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
-const {getDashboard} = require('../controller/appController');
+const {
+  getDashboard,
+  postAddAccount,
+  getAccounts,
+} = require('../controller/appController');
 
 //@desc Render the dashboard
 //@route GET /app/dashboard
 router.get('/dashboard', getDashboard);
+
+//@desc Save new account data to the Accounts Modal
+//@route POST /app/add-account
+router.post('/add-account', postAddAccount);
+
+//@desc Render the account management scree
+//@route GET /app/accounts
+router.get('/accounts', getAccounts);
 
 module.exports = router;
